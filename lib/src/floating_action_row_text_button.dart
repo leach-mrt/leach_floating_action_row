b851f08f28710590ad42d2ad944a478bfcd8f126
Mr.T <59334401+leach-mrt@users.noreply.github.com>
2021-03-29 21:48:09 +0900

    Create floating_action_row_text_button.dart

---
+++ lib/src/floating_action_row_text_button.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+/// Creates a button for [FloatingActionRow]
+class FloatingActionRowTextButton extends StatelessWidget {
+  final Widget text;
+
+  /// Background color
+  final Color color;
+
+  /// Width and height of this widget
+  final double? size;
+  final EdgeInsets padding;
+  final ShapeBorder? shape;
+
+  /// Foreground color for [icon]
+  final Color? foregroundColor;
+  final GestureTapCallback onTap;
+
+  FloatingActionRowButton({
+    required this.text,
+    this.color = Colors.transparent,
+    this.size,
+    this.padding = const EdgeInsets.all(0),
+    this.shape,
+    this.foregroundColor,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Color? defaultColor =
+        Theme.of(context).floatingActionButtonTheme.foregroundColor ??
+            Theme.of(context).accentIconTheme.color;
+
+    Widget result;
+
+    result = LayoutBuilder(
+      builder: (context, constraints) {
+        bool isColumn = false;
+        if (constraints.maxHeight > constraints.maxWidth) {
+          isColumn = true;
+        }
+        double defaultSize =
+            isColumn ? constraints.maxWidth : constraints.maxHeight;
+        return SizedBox(
+          width: size ?? defaultSize,
+          height: size ?? defaultSize,
+          child: Container(
+            child: RawMaterialButton(
+              elevation: 0,
+              highlightElevation: 0,
+              fillColor: color,
+              padding: padding,
+              shape: shape ??
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+              child: TextTheme.merge(
+                data: TextThemeData(
+                  color: foregroundColor ?? defaultColor,
+                ),
+                child: text,
+              ),
+              onPressed: onTap,
+            ),
+          ),
+        );
+      },
+    );
+
+    return result;
+  }
+}
