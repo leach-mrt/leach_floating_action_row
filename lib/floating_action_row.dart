f7a335eca3f8c5bff09e82bfcb0cf35d1d98ed9c
Mr.T <takuya1.tominaga@leach.tokyo>
2021-03-29 21:57:36 +0900

    Update floating_action_row.dart

--- lib/floating_action_row.dart
+++ lib/floating_action_row.dart
@@ -2,4 +2,5 @@ library floating_action_row;
 
 export 'src/floating_action_row.dart';
 export 'src/floating_action_row_button.dart';
+export 'src/floating_action_row_text_button.dart';
 export 'src/floating_action_row_divider.dart';
